8ce1bdf50d2fa1d758bf05b1b86dd7d197b257ea
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-10-17 19:19:58 +0300

    Add baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 import theme from "theme";
 
@@ -8,6 +9,7 @@ import Header from "components/Header";
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Header />
       <div>App</div>
     </ThemeProvider>
