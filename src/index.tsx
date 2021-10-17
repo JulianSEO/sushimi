4adff5c7b017c46d4f535074e75fe4edeb33c3a8
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-10-17 19:00:20 +0300

    Add default entries

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from "./App";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-ReactDOM.render(<div />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
