20724475e7b9653ecb34501b15951f4f13fe9961
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-10-27 21:32:15 +0300

    Adjust layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,31 +1,17 @@
 import React from "react";
-import { Container, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 
 import Header from "components/Header";
-
-import mock from "config/mock";
-import ProductCard from "components/ProductCard";
-import HomePage from "pages/Home";
+import Subheader from "components/Subheader";
 
 const Layout = (props: any) => {
   return (
     <React.Fragment>
-      <Header />
-      <HomePage />
-      <Container sx={{ paddingTop: "24px" }}>
-        <div>
-          <Grid container spacing={2}>
-            {mock.items.map((item, index) => {
-              return (
-                <Grid item xs={12} md={4} key={index}>
-                  <ProductCard item={item} />
-                </Grid>
-              );
-            })}
-          </Grid>
-        </div>
-      </Container>
-      {props.child}
+      <Box>
+        <Header />
+        <Subheader />
+        {props.children}
+      </Box>
     </React.Fragment>
   );
 };
