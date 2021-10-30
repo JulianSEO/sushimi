dacae1d75682e78476b73aad7a4ce9dc7348abd7
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-10-30 20:09:18 +0300

    Update interface

--- packages/website/src/store/reducers/cart.reducer.ts
+++ packages/website/src/store/reducers/cart.reducer.ts
@@ -1,13 +1,13 @@
 import { combineReducers } from "redux";
 import { createReducer } from "typesafe-actions";
 
-import { Item } from "@sushimi/interfaces";
+import { Product } from "@sushimi/interfaces";
 import { RootAction } from "interfaces";
 
 import { cartAddItem } from "store/actions/cart.actions";
 
 const cartReducer = combineReducers({
-  items: createReducer<Item[], RootAction>([]).handleAction(
+  items: createReducer<Product[], RootAction>([]).handleAction(
     [cartAddItem],
     (state, action) => {
       return [...state, { description: "", price: 0, title: "" }];
