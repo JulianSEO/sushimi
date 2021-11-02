1016bac0fa184248a3b77dc17e1eccbf8056d612
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-11-02 19:35:37 +0300

    Add basic api

--- packages/core/src/product/product.controller.ts
+++ packages/core/src/product/product.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import { Product } from '@sushimi/interfaces';
 
 import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get('/')
+  async find(): Promise<Product[]> {
+    return [];
+  }
 }
