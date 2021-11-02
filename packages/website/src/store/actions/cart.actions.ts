0783af42c3af98bfd39e19c808b2e3e023396692
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-11-02 19:35:17 +0300

    Remove reducers

--- packages/website/src/store/actions/cart.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createStandardAction } from "typesafe-actions";
-
-export const cartAddItem = createStandardAction("@@cart/add")<string>();
-export const cartRemoveItem = createStandardAction("@@cart/remove")<string>();
