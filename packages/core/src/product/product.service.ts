63b6aabfed6b27e9621f83f4a3781b946505838c
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-11-03 15:30:55 +0300

    Adjust interfaces

--- packages/core/src/product/product.service.ts
+++ packages/core/src/product/product.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from '@sushimi/interfaces/lib';
 
 import mock from './mock';
 
 @Injectable()
 export class ProductService {
-  async find() {
-    return mock.items;
+  async find(): Promise<Product[]> {
+    return mock.items as unknown as Product[];
   }
 }
